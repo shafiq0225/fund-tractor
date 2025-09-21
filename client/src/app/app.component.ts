ac8854b8df7dab2a1209513ad6232def5429d21a
shafiq0225 <shafiqahamed.be@gmail.com>
2025-09-21 17:02:08 +0530

    intergrate splash

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,35 @@ import { LayoutComponent } from "./layout/layout.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected title = 'FundTracker';
+  private appRef = inject(ApplicationRef);
+  private splashMinTime = 1000; // minimum 1 second
+  private splashStartTime = Date.now();
+
+  ngOnInit() {
+    // Splash start time
+    this.splashStartTime = Date.now();
+
+    // Wait until app is stable
+    this.appRef.isStable
+      .pipe(first(stable => stable))
+      .subscribe(() => this.hideSplashWithMinTime());
+  }
+
+  private hideSplashWithMinTime() {
+    const elapsed = Date.now() - this.splashStartTime;
+    const remaining = Math.max(this.splashMinTime - elapsed, 0);
+
+    setTimeout(() => this.hideSplash(), remaining);
+  }
+
+  private hideSplash() {
+    const splash = document.getElementById('splash');
+    if (splash) {
+      splash.style.opacity = '0';
+      setTimeout(() => splash.remove(), 300); // match CSS transition
+    }
+  }
+
 }
