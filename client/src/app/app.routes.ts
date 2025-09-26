95b4cd150ea30eed50b8ea3db9561b0e7bd82d73
shafiq0225 <shafiqahamed.be@gmail.com>
2025-09-26 16:33:00 +0530

    add nav report componnet

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { NavComponent } from './features/nav/nav.component';
 import { NavImportComponent } from './features/nav/nav-import/nav-import.component';
 import { NavDashboardComponent } from './features/nav/nav-dashboard/nav-dashboard.component';
 import { ManageSchemesComponent } from './features/nav/manage-schemes/manage-schemes.component';
-import { NavReportComponent } from './core/features/nav-report/nav-report.component';
+import { NavReportComponent } from './features/nav/nav-report/nav-report.component';
 
 export const routes: Routes = [
     {
