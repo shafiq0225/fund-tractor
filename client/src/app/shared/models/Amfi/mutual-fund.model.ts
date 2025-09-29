a6af7cd1540031a0d5e175fa9fe4f3fe39029941
shafiq0225 <shafiqahamed.be@gmail.com>
2025-09-29 15:41:19 +0530

    nav added

---
+++ client/src/app/shared/models/Amfi/mutual-fund.model.ts
@@ -0,0 +1,15 @@
+// src/app/features/nav/nav-report/models/mutual-fund.model.ts
+export interface MutualFund {
+  id: number;
+  name: string;
+  type: string;
+  scheme: string;
+  rate1: number;
+  percent1: number;
+  rate2: number;
+  percent2: number;
+  change: number;
+  rank: number;
+  icon: string;
+  color: string;
+}
